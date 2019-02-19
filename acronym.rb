11706a45a4db0af160a00dc2531071564a806f96
Michael Hughes <mhughes36@live.co.uk>
2019-02-19 02:36:46 +0000

    Syntax fix

--- acronym.rb
+++ acronym.rb
@@ -2,7 +2,7 @@ class Acronym
     def self.abbreviate(string)
         array = string.gsub(/\W+/, " ").split(" ")
         acronym = ""
-        array.each do |item| {acronym << "#{item[0].upcase}"}
+        array.each {|item| acronym << "#{item[0].upcase}"}
         acronym
     end
 end
