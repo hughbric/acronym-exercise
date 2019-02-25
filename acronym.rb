670f4f03cc902d24d33c21bbffc51e48339477b7
Michael Hughes <mhughes36@live.co.uk>
2019-02-25 19:04:18 +0000

    Refactored with RegExp

--- acronym.rb
+++ acronym.rb
@@ -1,8 +1,5 @@
 class Acronym
-    def self.abbreviate(string)
-        array = string.gsub(/\W+/, " ").split(" ")
-        acronym = ""
-        array.each {|item| acronym << "#{item[0].upcase}"}
-        acronym
-    end
+  def self.abbreviate(string)
+    string.scan(/\b[a-zA-Z]/).join.upcase
+  end
 end
